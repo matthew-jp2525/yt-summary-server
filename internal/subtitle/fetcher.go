c40bf7caef36ff121197b17fdd99ae55f53ea993
matthew.jp2525 <matthew.jp2525@gmail.com>
2025-12-24 16:24:12 +0900

    Check input url.

--- internal/subtitle/fetcher.go
+++ internal/subtitle/fetcher.go
@@ -3,12 +3,36 @@ package subtitle
 import (
 	"context"
 	"errors"
+	"fmt"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
+func validateYoutubeURL(value string) error {
+	u, err := url.ParseRequestURI(value)
+	if err != nil {
+		return fmt.Errorf("invalid url: %w", err)
+	}
+
+	host := u.Hostname()
+
+	if host != "youtu.bu" &&
+		host != "youtube.com" &&
+		host != "www.youtube.com" &&
+		host != "m.youtube.com" {
+		return fmt.Errorf("not a youtube host: %s", host)
+	}
+
+	return nil
+}
+
 func FetchAndClean(ctx context.Context, url string) (string, error) {
+	if err := validateYoutubeURL(url); err != nil {
+		return "", err
+	}
+
 	tmpDir, err := os.MkdirTemp("", "yt-sub")
 	if err != nil {
 		return "", err
